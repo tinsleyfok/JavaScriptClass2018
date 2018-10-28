843a256849854ec8d560e2251e8325ced922f124
Tinsley Huo <tinsleyhuo@Tinsleys-MacBook-Pro.local>
2018-10-28 03:09:37 -0400

    midterm

--- midterm/scripts.js
+++ midterm/scripts.js
@@ -6,30 +6,36 @@ console.log("Tinsley");
     };
 
     this.search = function () {
-            
+
         var form = document.querySelector("#searchForm");
 
         form.addEventListener("submit", function (e) {
-            e.preventDefault(); 
+            e.preventDefault();
             var value = document.querySelector("#SearchBox").value;
-            
+
             form.reset();
 
             getData(value.split(' ').join("+"));
 
         });
-    }; 
+    };
     this.getData = function (artist) {
-        window.location.href="#song_list";
+        window.location.href = "#song_list";
         document.body.style.overflow = "scroll";
         var http = new XMLHttpRequest();
+
+        http.open(method, url);
+
+        http.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+        http.setRequestHeader("Access-Control-Allow-Origin", "*");
+
         artist = artist.toString();
         var url = "https://itunes.apple.com/search?term=" + artist + "&entity=song";
         var method = "GET";
         var container = document.querySelector('#song_list_container');
         container.innerHTML = '';
 
-        http.open(method, url); 
+        http.open(method, url);
         http.onreadystatechange = function () {
             if (http.readyState = 4 && http.status == 200) {
                 console.log("SUCCESS - connected to itunes API");
@@ -40,13 +46,13 @@ console.log("Tinsley");
             }
         }
         http.send();
-    }; 
+    };
 
 
 
 
     this.showArtist = function (obj) {
-   
+
         console.log(obj);
         var container = document.querySelector("#song_list_container");
         var template = '';
@@ -57,7 +63,7 @@ console.log("Tinsley");
                 template += '<li class = "col-sm-3 song_item">';
 
                 template += '<div class = "song_thmb" style = "background: url(' + obj.results[i].artworkUrl100 + ')" ></div>';
-//                onclick = "openMedia(' + obj.results[i].previewUrl + ')"
+                //                onclick = "openMedia(' + obj.results[i].previewUrl + ')"
 
                 template += '<div class="song">' + obj.results[i].trackName + '</div>';
 
@@ -65,12 +71,12 @@ console.log("Tinsley");
 
                 template += '<li>';
 
-//                function openMedia(url) {
-//                    if (!url) return;
-//                    const media = document.getElementsByClassName("song_thmb");
-//                    media.innerHTML = `<audio controls source src = ' + obj.results[i].previewUrl + '></audio>`;
-//                }
-      
+                //                function openMedia(url) {
+                //                    if (!url) return;
+                //                    const media = document.getElementsByClassName("song_thmb");
+                //                    media.innerHTML = `<audio controls source src = ' + obj.results[i].previewUrl + '></audio>`;
+                //                }
+
             }
             container.innerHTML = '';
             container.insertAdjacentHTML('afterbegin', template);
@@ -78,7 +84,7 @@ console.log("Tinsley");
             document.querySelector('#not_match').style.display = "block";
         }
 
-    }; 
+    };
 
     this.init();
 })();
